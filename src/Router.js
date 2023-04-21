c7a8b290a889ea6a4af31e16ad24f7d20abe152d
hoinlee-moi <lhi3048@gmail.com>
2023-04-21 22:10:47 +0900

    feat: 그라데이션 로딩 만들기

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoadingBoard from "./pages/borad/LoadingBoard";
 import Loading1 from "./pages/loading/Loading1";
 import Loading2 from "./pages/loading/Loading2";
+import Loading3 from "./pages/loading/Loading3";
 import Main from "./pages/main/Main";
 
 const Router = () => {
@@ -13,6 +14,7 @@ const Router = () => {
       children: [
         { path: "1", element: <Loading1 /> },
         { path: "2", element: <Loading2 /> },
+        { path: "3", element: <Loading3 /> },
       ],
     },
   ]);
--- src/pages/borad/LoadingBoard.tsx
+++ src/pages/borad/LoadingBoard.tsx
@@ -3,11 +3,13 @@ import { Link, useNavigate, useRoutes } from "react-router-dom";
 import styles from "../../styles/Loading/Loading.module.css";
 import Loading1 from "../loading/Loading1";
 import Loading2 from "../loading/Loading2";
+import Loading3 from "../loading/Loading3";
 
 const LoadingBoard = () => {
   const element = useRoutes([
     { path: "1", element: <Loading1 /> },
     { path: "2", element: <Loading2 /> },
+    { path: "3", element: <Loading3 /> },
   ]);
   const navigate = useNavigate();
   return (
@@ -15,8 +17,9 @@ const LoadingBoard = () => {
       <h2>Loading Board</h2>
       <nav className={styles.boardNav}>
         <ul>
-          <li onClick={() => navigate("/loadingBoard/1")}>CIRCLE</li>
+          <li onClick={() => navigate("/loadingBoard/1")}>Circle</li>
           <li onClick={() => navigate("/loadingBoard/2")}>Square</li>
+          <li onClick={() => navigate("/loadingBoard/3")}>Gradation</li>
         </ul>
       </nav>
       <section className={styles.item_section}>{element}</section>
