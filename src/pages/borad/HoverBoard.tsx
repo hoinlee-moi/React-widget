a7f42a36723c115670f70429d5e203c9ff8a4cac
hoinlee-moi <lhi3048@gmail.com>
2023-05-06 13:49:23 +0900

    add : HoverTopDown 애니메이션 추가

--- src/pages/borad/HoverBoard.tsx
+++ src/pages/borad/HoverBoard.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import { useNavigate, useRoutes } from "react-router-dom";
 import styles from "../../styles/Board.module.css";
 import Dropdown1 from "../hover/Dropdown1";
+import Hover3DAni from "../hover/Hover3DAni";
 import HoverDetail from "../hover/HoverDetail";
 import HoverToolTip from "../hover/HoverToolTip";
 import OverNavi from "../hover/OverNavi";
+import TopDownNavi from "../hover/TopDownNavi";
 
 const HoverBoard = () => {
   const element = useRoutes([
@@ -12,6 +14,9 @@ const HoverBoard = () => {
     { path: "2", element: <HoverDetail /> },
     { path: "3", element: <HoverToolTip /> },
     { path: "4", element: <OverNavi /> },
+    { path: "5", element: <TopDownNavi /> },
+    { path: "6", element: <Hover3DAni /> },
+
   ]);
 
   const navigate = useNavigate();
@@ -25,6 +30,8 @@ const HoverBoard = () => {
           <li onClick={() => navigate("/hoverBoard/2")}>Hover Detail</li>
           <li onClick={() => navigate("/hoverBoard/3")}>Hover ToolTip</li>
           <li onClick={() => navigate("/hoverBoard/4")}>Over Navi</li>
+          <li onClick={() => navigate("/hoverBoard/5")}>TopDown Navi</li>
+          <li onClick={() => navigate("/hoverBoard/6")}>Hover3DAni</li>
         </ul>
       </nav>
       <section className={styles.item_section}>{element}</section>
---
+++ src/pages/hover/Hover3DAni.tsx
@@ -0,0 +1,8 @@
+const Hover3DAni = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+export default Hover3DAni
